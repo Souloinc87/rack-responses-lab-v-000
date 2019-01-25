9d9fddac29972b091410e120af9abeafb9c7b249
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-25 02:54:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     current_time = Time.now
 
-    if current_time >= 12
+    if Time.now.hour >= 12
       resp.write "Good Afternoon"
     else
       resp.write "Good Morning"
